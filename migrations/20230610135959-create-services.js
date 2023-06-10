4790cfe30eced11c4a6fe25d6731f26ca1f1229f
xIMet3 <joaquin19_91@hotmail.com>
2023-06-10 18:50:39 +0200

    [feat]Create seeders for users, services and appointments. // Creo los seeders de usuarios, servicios y citas.

--- migrations/20230610135959-create-services.js
+++ migrations/20230610135959-create-services.js
@@ -19,7 +19,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references:{
+          model: "Users",
+          key: "id"
+        }
       },
       createdAt: {
         allowNull: false,
