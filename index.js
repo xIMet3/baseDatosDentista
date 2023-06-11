30238d0dc4a1763e3e349bd0e8d2cc6b0bff1917
xIMet3 <joaquin19_91@hotmail.com>
2023-06-12 01:15:20 +0200

    [feat]Create the CRUD of the Roles table. Also create in postman all the endpoints needed for the project. // Creo el CRUD de la tabla Roles. Tambien creo en postman todos los endpoints necesarios para el proyecto.

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
-const express = require ('express');
-const app = express ();
+const express = require("express");
+const db = require("./db");
+const router = require("./router");
+
+const app = express();
+
 const PORT = 3000;
-app.use (express.json());
-app.listen (PORT, () => {
-    console.log("Servidor levantado en el puerto " + PORT);
+
+app.listen(PORT, () => {
+  console.log("Servidor levantado en el puerto " + PORT);
 });
+
+app.use(express.json());
+
+
+app.get('/role', (req, res) => {
+    return res.json(
+        {
+            succes: true,
+            message: "Get All Roles"
+        }
+    )
+});
+
+app.post('/role', (req, res) => {
+    return res.json(
+        {
+            succes: true,
+            message: "Create roles"
+        }
+    )
+});
+
+app.put('/role', (req, res) => {
+    return res.json(
+        {
+            succes: true,
+            message: "Update Roles"
+        }
+    )
+});
+
+app.delete('/role', (req, res) => {
+    return res.json(
+        {
+            succes: true,
+            message: "Delete Roles"
+        }
+    )
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
